7ec704a14ff9cfc36b804582a1e6236fc746a678
siddharth kathuria <kathuria.siddharth@yahoo.in>
2018-04-05 04:59:36 +0530

    arranged

--- app/src/main/java/com/example/sidkathuria14/symptomchecker/FirstActivity.java
+++ app/src/main/java/com/example/sidkathuria14/symptomchecker/FirstActivity.java
@@ -1,7 +1,10 @@
 package com.example.sidkathuria14.symptomchecker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -10,7 +13,30 @@ public class FirstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
 
-        
+        ((Button)findViewById(R.id.medicines)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(FirstActivity.this,MedicinesActivity.class));
+            }
+        });
+        ((Button)findViewById(R.id.disease)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(FirstActivity.this,MainActivity.class));
+            }
+        });
+        ((Button)findViewById(R.id.morse)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(FirstActivity.this,Morse.class));
+            }
+        });
+        ((Button)findViewById(R.id.tts)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(FirstActivity.this,tts.class));
+            }
+        });
 
     }
 }
